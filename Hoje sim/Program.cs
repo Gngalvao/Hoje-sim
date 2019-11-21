62417c39108074f55d14fce612ab95547c4d9a8d
Gngalvao <geovana_732@hotmail.com>
2019-11-21 14:15:57 -0300

    Criando um programa que obtem o nome do usuário.

---
+++ Hoje sim/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hoje_sim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nOlá, por favor digite o seu nome!");
+            Console.ReadLine();
+            Console.WriteLine("Digite qualquer tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
